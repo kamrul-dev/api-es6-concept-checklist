8c8becbc5a45f1d90e3bab75150709eeaa06814f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-27 15:35:45 +0600

    [Problem Solved 3.1] : return 89 in arrow function without parameter

---
+++ problem-solved3.1.js
@@ -0,0 +1,7 @@
+/* 
+    ৩.১. শূন্য প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে যেটা ৮৯ রিটার্ন করবে।
+*/
+
+// return 89 in arrow function without parameter
+const result = () => 89;
+console.log(result());
