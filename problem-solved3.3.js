77eb2319fad01ff941510729d894252fd8e3447d
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-27 15:58:14 +0600

    [Problem Solved 3.3] : write arrow function with two parameters

---
+++ problem-solved3.3.js
@@ -0,0 +1,9 @@
+/* 
+    ৩.৩ দুই, প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। এই ফাংশনের কাজ হবে যে দুইটা প্যারামিটার ইনপুট নিবে। সেই দুইটা প্যারামিটারকে যোগ করে যোগফলকে দুই দিয়ে ভাগ করে ভাগফল রিটার্ণ করে দাও।
+*/
+
+// write arrow function with two parameters and add two parameter then divide by 2 then return the result
+const addDivide = (num1, num2) => (num1 + num2) / 2;
+console.log(addDivide(20, 30));
+
+
