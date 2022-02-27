adcb72ca4ec5e3fcd6b81f9fc131bec86c7d21d3
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-27 15:40:44 +0600

    [Problem - Solved 3.2] : given number divide by 7 using arrow function

---
+++ problem-solved3.2.js
@@ -0,0 +1,7 @@
+/* 
+    ৩.২. এক প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন ডিক্লেয়ার করবে। এই ফাংশনের কাজ হবে যে প্যারামিটার নিবে সেটাকে ৭ দিয়ে ভাগ করে ভাগফল রিটার্ন করবে। 
+*/
+
+// given number divide by 7 using arrow function
+const divideBySeven = (number) => number / 7;
+console.log(divideBySeven(700));
